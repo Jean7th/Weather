bd009fdd941e4cbec8393c03d77fc5aabad6d343
Jirat <jirat7439@hotmail.com>
2024-09-25 17:16:10 +0100

    Updated

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ var desc = document.getElementById('description');
 const matchList = document.getElementById('match-list');
 var arrayCitites = [];
 
+// Get Real DateTime
+var timeDisplay = document.getElementById("datetime");
+
+var time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone //Get user corrct timezone
+var userLang = navigator.language || navigator.userLanguage; //Get user correct language
+
+function refreshTime() {
+    var dateString = new Date().toLocaleString(userLang, time_zone);
+    var formattedString = dateString.replace(", ", " - ");
+    timeDisplay.innerHTML = formattedString;
+}
+
+setInterval(refreshTime, 1000); //Refresh Time
+
 //Button Listener
 searchButton.addEventListener('click', function() {
     //Fetch API
@@ -34,6 +48,58 @@ document.addEventListener("keypress", function(event) {
         .catch(error => alert(`\u26A0 Please enter a correct city name.`));
     }
 })
+
+// Night Sky element for Background ===================================
+
+const $el = document.body;
+
+// Generate a random number between min and max values
+
+const genRandomNumber = (min, max) => {
+    return Math.random() * (max - min) + min;
+}
+
+// Generate a star <div>
+
+const genStar = () => {
+
+    const star = document.createElement("div");
+    star.classList.add("star");
+
+    // Gen star coordinates relative to $el size
+    let x = genRandomNumber(1, $el.offsetWidth);
+    let y = genRandomNumber(1, $el.offsetHeight);
+
+    const { style } = star;
+
+    style.left = Math.floor(x) + "px";
+    style.top = Math.floor(y) + "px";
+
+    style.setProperty(
+        "--star-size",
+        genRandomNumber(1, 3) + "px"
+    );
+
+    style.setProperty(
+        "--twinkle-duration",
+        Math.ceil(genRandomNumber(1, 5)) + "s"
+    );
+
+    style.setProperty(
+        "--twinkle-delay",
+        Math.ceil(genRandomNumber(1, 5)) + "s"
+    );
+
+    return star;
+}
+
+for (let index = 0; index < 200; index++) {
+    $el.append(genStar());
+}
+
+window.addEventListener("load", function(event) {
+    document.body.classList.remove('no-fouc');
+});
 
 // //Autocomplete Function search cities500.json
 // const searchCity = async searchText => {
